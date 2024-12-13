64ef71e5616d81fa4c803e53e529fda51fc0c089
Mouse Suicide <mrnikson.bel@gmail.com>
2024-12-13 18:50:10 +0300

    Добавьте файлы проекта.

---
+++ Pelmeni))))/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program()
+{
+    public static int Main()
+    {
+        //Первая задача
+        
+        double Ptln = inputDouble("Введите кол-во суточной выработки пельменей))), т: ") / 16;
+        double Ppa = inputDouble("Введите производительность пельменного автомата, т/ч: ");
+        int n1 = (int)Math.Ceiling(Ptln / Ppa);
+        Console.WriteLine("Необходимое количество пельменных автоматов: " + n1);
+        Console.ReadLine();
+        Console.Clear();
+
+
+        return 0;
+    }
+
+
+    private static double inputDouble(string str)
+    {
+        double value = -1;
+        bool isDouble = false;
+        while (!isDouble)
+        {
+            Console.Write(str);
+            isDouble = double.TryParse(Console.ReadLine(), out value);
+        }
+        return value;
+    }
+
+}
